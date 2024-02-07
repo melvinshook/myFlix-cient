47375b209d75d34720c4fba7f8ef040766e98506
melvinshook <139077191+melvinshook@users.noreply.github.com>
2024-02-07 04:54:20 -0500

    updated
    added useEffect to fetch data from API

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,39 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";  
 
 
 
-
 export const MainView = () => {
-    const [movies, setMovies] = useState([
-        {
-            id: 1,
-            title: "Step Brothers",
-            image: "https://www.sonypictures.com/sites/default/files/styles/max_560x840/public/title-key-art/450688_StepBrothers_2008_1400x2100_US.jpg?itok=qfBBSVlL",
-            description: "Two aimless middle-aged losers still living at home are forced against their will to become roommates when their parents marry.",
-            genre: "Comedy",
-            director: "Adam McKay"
-        },
-        {
-            id: 2,
-            title: "Pulp Ficton",
-            image: "https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_FMjpg_UX1055_.jpg",
-            description: "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
-            genre: "Drama",
-            director: "Quentin Tarantino"
-        },
-        {
-            id: 3,
-            title: "Fight Club",
-            image: "https://m.media-amazon.com/images/M/MV5BY2MxZmJiY2YtMGYyNy00ZmFmLWE5ODQtZTY3YTZlNTE4OTk0XkEyXkFqcGdeQXVyNjUxNDQwMzA@._V1_FMjpg_UX404_.jpg",
-            description: "An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more.",
-            genre: "Drama",
-            director: "David Fincher"
-        }
-    ]);
+    const [movies, setMovies] = useState([]);
+
+
 
     const [selectedMovie, setSelectedMovie] = useState(null);
+
+    useEffect(() => {
+        fetch("https://movie-api-careerfoundry-b3e87d3aa42c.herokuapp.com/movies")
+        .then ((response) => response.json())
+        .then((data) => {
+            console.log("movies from api:", data);
+            const moviesFromApi = data.map((movie) => {
+                return { 
+                    _id: movie._id,
+                    title: movie.title,
+                    description: movie.description,
+                    genre: {
+                      name: movie.genre.name,
+                      description: movie.genre.description
+                    },
+                    director: {
+                      name: movie.director.name,
+                      bio: movie.director.bio
+                    },
+                    actors: movie.actors,
+                    imagePath: movie.imagePath
+                    
+                }
+            });
+            setMovies(moviesFromApi);
+                });
+        }, []);
 
     if (selectedMovie) {
         return (
