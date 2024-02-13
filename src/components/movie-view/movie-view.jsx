9550484730c3779ddef2bc3f6765175d10151408
melvinshook <139077191+melvinshook@users.noreply.github.com>
2024-02-12 19:52:42 -0500

    updated movie view

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,34 +1,42 @@
-export const MovieView = ({movie, onBackClick }) => {
-    return (
-        <div>
-            <div>
-                <img src={movie.imagePath} width="300" height="500"  />
-            </div>
-            <div>
-                <span>Title: </span>
-                <span>{movie.title}</span>
-            </div>
-            <div>
-            <span>Description: </span>
-            <span>{movie.description}</span>
-            </div>
-            <div>
-                <span>Genre: </span>
-                <span>{movie.genre.name}</span>
-            </div>
-            <div>
-            <span>Description:</span>
-            <span>{movie.genre.description}</span>
-            </div>
-            <div>
-                <span>Director: </span>
-                <span>{movie.director.name}</span>
-            </div>
-            <div>
-             <span>Bio:</span>   
-            <span>{movie.director.bio}</span>
-            </div>
-            <button onClick={onBackClick}>Back</button>
-        </div>
-    );
+import "./movie-view.scss";
+
+export const MovieView = ({ movie, onBackClick }) => {
+  return (
+    <div>
+      <div>
+        <img className="w-50" src={movie.imagePath} />
+      </div>
+      <div>
+        <span>Title: </span>
+        <span>{movie.title}</span>
+      </div>
+      <div>
+        <span>Description: </span>
+        <span>{movie.description}</span>
+      </div>
+      <div>
+        <span>Genre: </span>
+        <span>{movie.genre.name}</span>
+      </div>
+      <div>
+        <span>Description:</span>
+        <span>{movie.genre.description}</span>
+      </div>
+      <div>
+        <span>Director: </span>
+        <span>{movie.director.name}</span>
+      </div>
+      <div>
+        <span>Bio:</span>
+        <span>{movie.director.bio}</span>
+      </div>
+      <button
+        onClick={onBackClick}
+        className="back-button"
+        style={{ cursor: "pointer" }}
+      >
+        Back
+      </button>
+    </div>
+  );
 };
