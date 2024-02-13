74dedcf604dec89e22a63bb55379864ef81c94c1
melvinshook <139077191+melvinshook@users.noreply.github.com>
2024-02-12 19:51:42 -0500

    added container

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,21 @@
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
 // Import statement to indicate that you need to bundle './index.scss'
 import "./index.scss";
+import Container from "react-bootstrap/Container";
 
 // Main component (will eventually use all the others)
 const MyFlixApplication = () => {
-    return <MainView />;
-    
+  return (
+    <Container>
+      <MainView />
+    </Container>
+  );
 };
-
 
 // Finds the root of your app
 const container = document.querySelector("#root");
 const root = createRoot(container);
 
-// Tells React to render your app in the root DOM element 
+// Tells React to render your app in the root DOM element
 root.render(<MyFlixApplication />);
